e961a6ab1710441563b56a958d9407179351ea33
Daniel diaz <125688150+Dragunovich1@users.noreply.github.com>
2023-08-20 14:56:33 -0300

    Update setup.py
    Se agrega direccion de git al setup.py

--- segunda-pre-entrega-daniel-diaz/setup.py
+++ segunda-pre-entrega-daniel-diaz/setup.py
@@ -7,10 +7,9 @@ setup(
     author="Daniel Diaz",
     author_email="danielciarlantini@gmail.com",
     description="Paquete para modelar clientes en una página de compras",
-    url="https://github.com/tuusuario/tuproyecto",
+    url="https://github.com/Dragunovich1/coder",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Windows 11",
     ],
 )
