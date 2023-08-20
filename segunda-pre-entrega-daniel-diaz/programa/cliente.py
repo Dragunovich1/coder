a4335389be5733601228bc4266c0b29b5e6f06b1
Daniel diaz <danielciarlantini@gmail.com>
2023-08-20 14:51:31 -0300

    Python Coder
    Aqui dejaremos las entregas

---
+++ segunda-pre-entrega-daniel-diaz/programa/cliente.py
@@ -0,0 +1,32 @@
+# Archivo: cliente.py
+
+class Cliente:
+    def __init__(self, nombre, email, saldo, edad):
+        self.nombre = nombre
+        self.email = email
+        self.saldo = saldo
+        self.edad = edad
+
+    def realizar_compra(self, monto):
+        if monto <= self.saldo:
+            self.saldo -= monto
+            return f"Compra exitosa. Saldo restante: {self.saldo}"
+        else:
+            return "Saldo insuficiente para realizar la compra"
+
+    def recargar_saldo(self, monto):
+        self.saldo += monto
+        return f"Saldo recargado. Saldo actual: {self.saldo}"
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}\nEmail: {self.email}\nSaldo: {self.saldo}\nEdad: {self.edad}"
+
+# Clase derivada (subclase) utilizando herencia
+class ClientePremium(Cliente):
+    def __init__(self, nombre, email, saldo, edad, descuento):
+        super().__init__(nombre, email, saldo, edad)
+        self.descuento = descuento
+
+    def aplicar_descuento(self, monto):
+        monto_con_descuento = monto * (1 - self.descuento / 100)
+        return f"Descuento aplicado. Monto con descuento: {monto_con_descuento}"
---
+++ segunda-pre-entrega-daniel-diaz/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="segunda_pre-entrega_daniel_diaz",
+    version="1.0",
+    packages=find_packages(),
+    author="Daniel Diaz",
+    author_email="danielciarlantini@gmail.com",
+    description="Paquete para modelar clientes en una página de compras",
+    url="https://github.com/tuusuario/tuproyecto",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
